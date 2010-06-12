8a347790f3febb889c80a43505d7a5b37c1e15b1
Ian Page Hands <iphands@gmail.com>
2010-06-11 22:56:05 -0400

    rocketLauncher.c: added command line control, and refactored some things

--- rocketLauncher.c
+++ rocketLauncher.c
@@ -3,15 +3,35 @@
 #include <unistd.h>
 #include <ncurses.h>
 
-void do_ui(usb_dev_handle *launcher);
+#define DOWN 1
+#define UP 2
+#define LEFT 4
+#define RIGHT 8
+#define FIRE 16
+#define STOP 32
+
+void do_cli(usb_dev_handle *launcher, char direction, int timeout);
+char do_arg_check(int argc, char ** argv);
+void do_tui(usb_dev_handle *launcher);
 void disarm_rl(usb_dev_handle *launcher);
 void stop_rl(usb_dev_handle *launcher);
 void move_rl(usb_dev_handle *launcher, int sig);
 void send_sig(usb_dev_handle *launcher, int sig);
 struct usb_dev_handle* get_rocker_launcher_dev_handle();
 
-int main()
+int main(int argc, char ** argv)
 {
+
+  if (argc > 1) {
+    if(do_arg_check(argc, argv)) {
+      printf("Usage: %s [COMMAND TIMEOUT]\n\nCommands are:\n\tup\n\tdown\n\tleft\n\tright\n\tfire\n\tstop\n\n", argv[0]);
+      printf("Timeout is a number of miliseconds from 0 - 10000\n\n");
+      printf("Or use no args for an interactive version. In the interactive version the controls are:");
+      printf("\n\tup arrow\n\tdown arrow\n\tleft arrow\n\tright arrow\n\tspace bar (to fire)\n\tescape (to stop)\n\tctrl+c (to quit)\n\n");
+      return(1);
+    }
+  }
+
   struct usb_dev_handle *launcher = get_rocker_launcher_dev_handle();
   
   if (launcher == NULL) {
@@ -28,8 +48,14 @@ int main()
     return(1);
   }
   
-  do_ui(launcher);
-  
+  if (argc > 1) {
+    char direction = argv[1][0];
+    int timeout = (atoi(argv[2]) * 1000);
+    do_cli(launcher, direction, timeout);
+  } else {
+    do_tui(launcher);
+  }
+
   disarm_rl(launcher);
   stop_rl(launcher);
 
@@ -38,7 +64,71 @@ int main()
   return(0);
 }
 
-void do_ui(usb_dev_handle *launcher)
+void do_cli(usb_dev_handle *launcher, char direction, int timeout)
+{
+  switch (direction) {
+  case 'u':
+    send_sig(launcher, UP);
+    break;
+  case 'd':
+    send_sig(launcher, DOWN);
+    break;
+  case 'l':
+    send_sig(launcher, LEFT);
+    break;
+  case 'r':
+    send_sig(launcher, RIGHT);
+    break;
+  case 'f':
+    send_sig(launcher, FIRE);
+    break;
+  case 's':
+    stop_rl(launcher);
+    break;
+  default:
+    return;
+    break;
+  }
+  
+  usleep(timeout);
+  disarm_rl(launcher);
+  stop_rl(launcher);
+  return;  
+}
+
+char do_arg_check(int argc, char ** argv)
+{
+  if (argc != 3) {
+    return(1);
+  }
+
+  switch (argv[1][0]) {
+  case 'u':
+    break;
+  case 'd':
+    break;
+  case 'l':
+    break;
+  case 'r':
+    break;
+  case 'f':
+    break;
+  case 's':
+    break;
+  default:
+    return(1);
+    break;
+  }
+  
+  int time = atoi(argv[2]);
+  if ((time > 10000) || (time < 0)) {
+    return(1);
+  }
+
+  return(0);
+}
+
+void do_tui(usb_dev_handle *launcher)
 {
   initscr();
   raw();
@@ -52,25 +142,26 @@ void do_ui(usb_dev_handle *launcher)
     switch (key) {
     case KEY_UP:
       printw("up\n");
-      move_rl(launcher, 2);
+      move_rl(launcher, UP);
       break;
     case KEY_DOWN:
       printw("down\n");
-      move_rl(launcher, 1);
+      move_rl(launcher, DOWN);
       break;
     case KEY_LEFT:
       printw("left\n");
-      move_rl(launcher, 4);
+      move_rl(launcher, LEFT);
       break;
     case KEY_RIGHT:
       printw("right\n");
-      move_rl(launcher, 8);
+      move_rl(launcher, RIGHT);
       break;
     case 32:
       printw("FIRE\n");
-      send_sig(launcher, 16);
+      send_sig(launcher, FIRE);
       break;
     case 27:
+      printw("stop\n");
       disarm_rl(launcher);
       stop_rl(launcher);
       break;
@@ -78,12 +169,13 @@ void do_ui(usb_dev_handle *launcher)
   }
 
   endwin();
+  return;
 }
 
 void disarm_rl(usb_dev_handle *launcher)
 {
-   send_sig(launcher, 1);
-   return;
+  send_sig(launcher, 1);
+  return;
 }
 
 void stop_rl(usb_dev_handle *launcher)
