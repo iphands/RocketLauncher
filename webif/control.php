5576652a298c5e160507e229b87ea2b8c4d1173b
Ian Page Hands <iphands@gmail.com>
2010-06-14 19:59:24 -0400

    webif: added additional locking

--- webif/control.php
+++ webif/control.php
@@ -14,8 +14,14 @@ if (($time == NULL) || ($direction == NULL)) {
 }
 
 print($direction . " " . $time . "\n");
-$out = shell_exec('pgrep rocketLauncher >/dev/null 2>&1 ; echo $?');
-if ($out != 0) {
+
+$ps_out = shell_exec('pgrep rocketLauncher >/dev/null 2>&1 ; echo $?');
+$ls_out = shell_exec('cat ./lockfile >/dev/null 2>&1 ; echo $?');
+
+print("ps: " . $ps_out);
+print("ls: " . $ls_out);
+
+if (($ps_out = 0) || ($ls_out != 0)) {
   $out = shell_exec('/usr/local/sbin/rocketLauncher ' . $direction . ' ' . $time);
   $out = "<br>\n" . preg_replace('/\n/', '<br>', $out);
   print($out);
